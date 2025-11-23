edfa5136a41826af69d353a54638006824ed3a4c
keyner <keinerf588@gmail.com>
2025-11-23 14:39:19 -0500

    Ej4: Objeto celular

---
+++ scripts/Ej4.js
@@ -0,0 +1,16 @@
+/* Características de un celular: Cree un objeto llamado celular con propiedades:
+marca, modelo, año y estado. Muestre cada propiedad en consola de manera
+individual. */
+
+const celular = {
+  marca: "Samsung",
+  modelo: "Galaxy S23",
+  año: 2023,
+  estado: "Nuevo",
+};
+
+// Mostrar cada propiedad de manera individual
+console.log("Marca:", celular.marca);
+console.log("Modelo:", celular.modelo);
+console.log("Año:", celular.año);
+console.log("Estado:", celular.estado);
